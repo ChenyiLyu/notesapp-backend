28d946d19a9c9d3a5caee5e7a84ba07a7f4d0bc2
Chenyi Lyu <clyu4@jhu.edu>
2021-08-07 12:34:24 -0400

    initial commit

---
+++ src/main/java/in/evalyu/springboot/repository/NotesRepository.java
@@ -0,0 +1,11 @@
+package in.evalyu.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import in.evalyu.springboot.entity.Note;
+
+@Repository
+public interface NotesRepository extends JpaRepository<Note, Long> {
+
+}
