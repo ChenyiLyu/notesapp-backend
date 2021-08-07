a07a249a9e6ec192359b6085199277b653a56b29
Chenyi Lyu <clyu4@jhu.edu>
2021-08-07 15:03:55 -0400

    create rest end point for new note

--- src/main/java/in/evalyu/springboot/controller/NotesController.java
+++ src/main/java/in/evalyu/springboot/controller/NotesController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +19,21 @@ import in.evalyu.springboot.repository.NotesRepository;
 @RequestMapping("/api")
 @CrossOrigin("*")
 public class NotesController {
+	
 	@Autowired
 	NotesRepository nRepo;
 	
 	@GetMapping("/notes")
-	public ResponseEntity<List<Note>> readNotes(){
+	public ResponseEntity<List<Note>> readNotes () {
 		return new ResponseEntity<List<Note>>(nRepo.findAll(), HttpStatus.OK);
 	}
+	
+	@PostMapping("/notes")
+	public ResponseEntity<Note> createNote(@RequestBody Note note) {
+		return new ResponseEntity<Note>(nRepo.save(note), HttpStatus.CREATED);
+	}
 }
+
+
+
+
